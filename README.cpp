afd87bd68c0b705d57121360106ac2c40f989adf
Rohit Singh <103178517+Rohit-Kandari@users.noreply.github.com>
2022-04-07 16:46:12 +0530

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -12,6 +12,16 @@ Button background will change automatically with random colors or second is with
 		var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 		var color = "#" + randomColor;
 		doc.style.backgroundColor = color;
+	}
+setInterval(change, 1000);
+
+//<----------------------- Or if you want to use array then you can do like this----------------> 
+
+	var i = 0;
+	function change() {
+		var doc = document.getElementById("background");
+		var color = ['red','green','yellow'];
+		doc.style.backgroundColor = color[i];
 		i = (i + 1) % color.length;
 	}
 setInterval(change, 1000);
