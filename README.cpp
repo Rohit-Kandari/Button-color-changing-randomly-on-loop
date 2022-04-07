aa73888ffbbe1c6243950c73b6dc1dff2ee449c7
Rohit Singh <103178517+Rohit-Kandari@users.noreply.github.com>
2022-04-07 16:16:44 +0530

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -3,14 +3,17 @@ Button background will change automatically with random colors or second is with
 
 
 <button type="button" id="background">i am Chameleons</button>
-	  <script type="text/javascript">    
-var i = 0;
-function change() {
-  var doc = document.getElementById("background");
-  var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  var color = "#" + randomColor;
-  doc.style.backgroundColor = color;
-  i = (i + 1) % color.length;
-}
+
+<script type="text/javascript">    
+
+	var i = 0;
+	function change() {
+		var doc = document.getElementById("background");
+		var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+		var color = "#" + randomColor;
+		doc.style.backgroundColor = color;
+		i = (i + 1) % color.length;
+	}
 setInterval(change, 1000);
+
 </script>
